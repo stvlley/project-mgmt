e04f9c933c60580a57cefd300240f54cc09be343
Stephen <stvlley.amzn@gmail.com>
2022-08-12 21:57:42 -0400

    lets 0001000100001100111100110001110010010001'''''

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,20 +1,13 @@
 import { gql, useQuery} from '@apollo/client';
 import ClientRow from './ClientRow';
-const GET_CLIENTS = gql`
-  query getClients {
-    clients {
-      id
-      name
-      email
-      phone
-    }
-  }
-`;
+import { GET_CLIENTS } from '../queries/clientQueries';
+import Spinner from './Spinner';
+
 export default function Clients() {
 
     const { loading, error, data} = useQuery(GET_CLIENTS)
 
-    if (loading) return <p>Loading...</p>
+    if (loading) return <Spinner />
     if (error) return <p>Something went wrong</p>
 
   return <>{!loading && !error && (
